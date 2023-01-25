fb0ee7d753be24343f16adc5d7105ab411c4c809
Shawn Annan <shawnrannan@gmail.com>
2023-01-25 00:43:25 +0000

    Add task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(
-        Error('The fake API is not working currently'),
-      );
-    }
+  const Mypromise = new Promise((resolve, reject) => {
+    resolve({
+      status: 200,
+      body: 'Success',
+    });
+    reject(
+      Error(),
+    );
+    promise.then(() => console.log('Got a response from the API'));
   });
+  return Mypromise;
 }
