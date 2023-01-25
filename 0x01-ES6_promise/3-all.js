8db5fa2637e0d6210be6a59cd1eb85b0cafe684f
Shawn Annan <shawnrannan@gmail.com>
2023-01-25 14:26:49 +0000

    Add task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  Promise.all([
+    promise1,
+    promise2,
+  ]).then((res) => { console.log(res[0].body, res[1].firstName, res[1].lastName); })
+    .catch(() => { console.log('Signup system offline'); });
+}
