db4e590fa2b38a1f5eb8c8b6dbe0916f94a3dc02
Shawn Annan <shawnrannan@gmail.com>
2023-01-25 14:55:48 +0000

    fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,13 +4,10 @@ export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
 
-  Promise.all([
+  return Promise.all([
     promise1,
     promise2,
   ]).then((res) => {
-    const fname = res[1].firstName;
-    const lname = res[1].lastName;
-    const bod = res[0].body;
-    console.log(`${bod} ${fname} ${lname}`);
+    console.log(res[0].body, res[1].firstName, res[1].lastName);
   }).catch(() => { console.log('Signup system offline'); });
 }
