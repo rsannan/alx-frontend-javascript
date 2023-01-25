34453b10058c8d9147306b60bbf66fcc08a248e2
Shawn Annan <shawnrannan@gmail.com>
2023-01-25 17:28:05 +0000

    Fix task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,8 +5,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
   const promises = [promise1, promise2];
-  const arr = [];
-  Promise.allSettled(promises).then((results) => {
+  return Promise.allSettled(promises).then((results) => {
+    const arr = [];
     results.forEach((result) => {
       const stat = result.status;
       let val = '';
