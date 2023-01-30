687917bcb74a283e1be15f92df9e4e47c30e704a
Shawn Annan <shawnrannan@gmail.com>
2023-01-30 15:51:06 +0000

    Add task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const obj = [];
+  obj.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  obj.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  obj.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return obj;
+}
