f01d42a0633b25d8aa8c7355cc0a62751aee82db
Shawn Annan <shawnrannan@gmail.com>
2023-01-31 01:38:34 +0000

    Fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const strlen = startString.length - 1;
-  if (strlen < 0 || !(typeof startString === 'string')) {
+  if (!(startString) || !(typeof startString === 'string')) {
     return '';
   }
   let str = '';
