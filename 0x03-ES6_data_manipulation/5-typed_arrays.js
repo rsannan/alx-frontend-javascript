c79a3c712a2a58ad59229f843b845c232c3f483c
Shawn Annan <shawnrannan@gmail.com>
2023-01-31 00:08:30 +0000

    Fix task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
 
   if (buffer.byteLength < position) {
-    return Error('Position outside range');
+    throw Error('Position outside range');
   }
 
   const arr = new DataView(buffer);
